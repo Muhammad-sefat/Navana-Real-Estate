1a48a8f4f6d93afb089d1bdfe3ce580ca7f0c8c9
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-04-10 12:59:49 +0600

    Add Footer

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,3 +1,4 @@
+import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import { Outlet } from "react-router-dom";
 
@@ -6,6 +7,7 @@ function App() {
     <div className="px-10 mx-auto">
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 }
