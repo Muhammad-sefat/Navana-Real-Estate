082c146438eb3a77a6c98e6fc04189fc022fb722
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-04-16 17:19:11 +0600

    Add Environment Variable

--- src/Components/FirebaseConfig.js
+++ src/Components/FirebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAk_p49WouBvTfSLv-I3v9Lw8JSeMf04mU",
-  authDomain: "navana-real-estate-965a9.firebaseapp.com",
-  projectId: "navana-real-estate-965a9",
-  storageBucket: "navana-real-estate-965a9.appspot.com",
-  messagingSenderId: "475158999709",
-  appId: "1:475158999709:web:d99cbe87b50396ba475a74",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
