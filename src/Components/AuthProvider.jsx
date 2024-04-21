48de3d538c2b8f2f7b6ec9a4e5a0c50a8e7d80ef
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-04-21 14:51:41 +0600

    Complete Private Route Section

--- src/Components/AuthProvider.jsx
+++ src/Components/AuthProvider.jsx
@@ -25,8 +25,8 @@ const AuthProvider = ({ children }) => {
     const unSubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         setUser(user);
-        setLoading(false);
       }
+      setLoading(false);
     });
     return () => {
       unSubscribe();
